42361aead2eaabf8ff30738a8158c89943262ed3
Isaac Bell <ibell@athn.org>
2021-08-29 17:20:36 -0600

    Add CSES "Range Update Queries"

---
+++ cses/range_update_queries.cpp
@@ -0,0 +1,155 @@
+// #include<bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <complex>
+#include <chrono>
+#include <functional>
+#include <climits>
+#include <utility>
+#include <limits>
+#include <random>
+#include <numeric>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+#define nl "\n"
+#define sz size
+#define rsz resize
+#define IMAX INT_MAX
+#define IMIN INT_MIN
+#define gc getchar_unlocked
+#define ll long long
+#define PI 3.1415926535897932384626
+#define INF 2000000000
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(s) printf("%s\n", s)
+#define br printf("\n")
+#define traverse(a, x) for (auto &a : x)
+#define fo(i, n) for(int i=0;i<n;i++)
+#define ford(i, n) for(int i = n - 1; i >= 0; i--)
+#define ford1(i, n) for(int i = n - 1; i; i--)
+#define Fo(i, k, n) for(int i = k; k < n ? i < n : i > n; k < n ? i += 1: i -= 1)
+#define deb(x) cout << #x << " = " << x << endl;
+#define deb2(x, y) cout << #x << " = " << x << ", " << #y << " = " << y << endl
+#define deba(i, a, n) fo(i, n){cout << a[i] << " ";}
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, x) for(auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) for(auto it = x.rbegin(); it != x.rend(); it+)
+
+typedef long double ld;
+typedef complex<ld> cd;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vl vll;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<ld> vd;
+typedef vector<cd> vcd;
+typedef vector<uint32_t> vu32;
+typedef vector<uint64_t> vu64;
+
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+  uniform_int_distribution<int> uid(0,lim-1);
+  return uid(rang);
+}
+
+template<class T> using pq = priority_queue<T>;
+template<class T> using mpq = priority_queue<T, vector<T>, greater<T>>;
+
+const int mod = 1'000'000'007;
+const int N = 3e5;
+
+template<typename T> T M(T x) { return ((x % mod + mod) % mod);  }
+template<typename T> T addM(T a, T b)  { return mod(mod(a) + mod(b));  }
+template<typename T> T multM(T a, T b) { return mod(mod(a) * mod(b));  }
+
+vi v[N];
+int a[N];
+
+bool ckmin(int& a, int b){ return b < a ? a = b, true : false; }
+bool ckmax(int& a, int b){ return b > a ? a = b, true : false; }
+
+/* Solution starts here */
+
+vl tree;
+ll n;
+
+ll query(ll i, ll lo, ll hi, ll a, ll b, ll v = -1) {
+  if (a <= lo && b >= hi) {
+    if (v != -1) tree[i] += v;
+    return tree[i];
+  }
+  if (hi < a || b < lo) return 0;
+  ll mid = (hi+lo)/2;
+  ll sum = query(2*i, lo, mid, a, b, v) + query(2*i+1, mid+1, hi, a, b, v);
+  return sum + tree[i];
+}
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+  srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+  ll q; cin >> n >> q;
+  vl A(n); fo(i, n) cin >> A[i];
+
+  while (__builtin_popcount(n) != 1) {
+    n++;
+    A.pb(0);
+  }
+
+  tree.rsz(2*n);
+  fo(i, n) tree[i+n] = A[i];
+  // ford1(i, n) tree[i] = tree[2*i]+tree[2*i+1];
+
+  while (q--) {
+    short int type;
+    cin >> type;
+    if (type == 1) {
+      ll a, b, v; cin >> a >> b >> v;
+
+      //update(a, b);
+      query(1, 0, n-1, --a, --b, v);
+    } else {
+      ll k; cin >> k;
+      k--;
+      cout << query(1, 0, n-1, k, k) << nl;
+    }
+  }
+
+  return 0;
+}
