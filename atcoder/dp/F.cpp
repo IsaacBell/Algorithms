149ab125602364f49941c6c01140e13dcca1411e
Isaac Bell <isaacbell388@gmail.com>
2021-10-23 16:41:10 -0500

    Add LCS algorithm w/ string printing

--- atcoder/dp/F.cpp
+++ atcoder/dp/F.cpp
@@ -1,5 +1,6 @@
 // #include<bits/stdc++.h>
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -13,6 +14,7 @@
 #include <map>
 #include <unordered_map>
 #include <set>
+#include <unordered_set>
 #include <complex>
 #include <chrono>
 #include <functional>
@@ -33,6 +35,8 @@ using namespace std;
 #define nl "\n"
 #define sz size
 #define rsz resize
+#define ret return
+#define cont continue
 #define IMAX INT_MAX
 #define IMIN INT_MIN
 #define gc getchar_unlocked
@@ -60,12 +64,22 @@ using namespace std;
 #define mt make_tuple
 #define F first
 #define S second
+#define FR front
+#define BK back
+#define qt(args...) auto [args] = q.top(); q.pop();
+#define qta(arg...) auto arg = q.top(); q.pop();
+#define qf(args...) auto [args] = q.front(); q.pop();
+#define qp(args...) q.push(args)
+#define qe q.empty()
+#define wqe while(!q.empty())
+#define begend(x) all(x)
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
 #define clr(x) memset(x, 0, sizeof(x))
 #define sortall(x) sort(all(x))
 #define tr(it, x) for(auto it = x.begin(); it != x.end(); it++)
 #define trr(it, x) for(auto it = x.rbegin(); it != x.rend(); it+)
+#define getunique(v) {sort(all(v)); v.erase(unique(all(v)), v.end());}
 
 typedef long double ld;
 typedef complex<ld> cd;
@@ -83,6 +97,9 @@ typedef vector<ld> vd;
 typedef vector<cd> vcd;
 typedef vector<uint32_t> vu32;
 typedef vector<uint64_t> vu64;
+typedef map<ll, ll> mll;
+typedef map<ll, string> mls;
+typedef map<string, ll> msl;
 
 mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
 int rng(int lim) {
@@ -90,18 +107,91 @@ int rng(int lim) {
   return uid(rang);
 }
 
-template<class T> using pq = priority_queue<T>;
-template<class T> using mpq = priority_queue<T, vector<T>, greater<T>>;
-
 const ll mod = 1e9 + 7;
 const ll N = 3e5;
 
-template<typename T> T M(T x) { return ((x % mod + mod) % mod);  }
-template<typename T> T addM(T a, T b)  { return mod(mod(a) + mod(b));  }
-template<typename T> T multM(T a, T b) { return mod(mod(a) * mod(b));  }
+template<typename T=ll> using ql  = queue<ll>;
+template<typename T=ll> using qp  = queue<pair<T,T>>;
+template<typename T=ll> using qt  = queue<tuple<T,T>>;
+template<typename T=ll> using qt3 = queue<tuple<T,T,T>>;
+template<typename T=ll> using qt4 = queue<tuple<T,T,T,T>>;
+template<typename T=ll> using pq  = priority_queue<T>;
+template<typename T=ll> using mpq = priority_queue<T, vector<T>, greater<T>>;
 
-template<typename T>
-T fac(T x) {
+using qll  = queue<ll>;
+using qpl  = qp<ll>;
+using qtl  = qt<ll>;
+using qtl3 = qt3<ll>;
+using qtl4 = qt4<ll>;
+using qpd  = qp<double>;
+using qtd  = qt<double>;
+using qtd3 = qt3<double>;
+using qtd4 = qt4<double>;
+
+// Modulo operators
+template<typename T=ll> T M(T x) { return ((x % mod + mod) % mod);  }
+template<typename T=ll> T addM(T a, T b)  { return M(M(a) + M(b)); }
+template<typename T=ll> T subM(T a, T b)  { a -= b; return a < 0 ? a + mod : a; }
+template<typename T=ll> T multM(T a, T b) { return M(M(a) * M(b)); }
+template<typename T=ll> T powM(T x, T y) {
+  T o = 1;
+  x %= mod;
+  while (y) {
+    if (y & 1) (o *= x) %= mod;
+    y >>= 1;
+    (x *= x) %= mod;
+  }
+  return o;
+}
+
+// General functions
+template<class T=ll> T gcd(T a, T b) { return b ? gcd(b, a%b) : a; }
+template<class T=ll> T lcm(T a, T b) { return a*(b/gcd(a,b)); }
+template<typename T=ll> bool ckmin(T& a, T b){ return b < a ? a = b, true : false; }
+template<typename T=ll> bool ckmax(T& a, T b){ return b > a ? a = b, true : false; }
+
+template<class T=ll> T maxpow2(T n) { return (n & (~(n - 1))); }
+template<class T=ll> T count_digit(T number) { return T(log10(number) + 1); }
+template<class T=ll> T sum_digit(T n) {
+  T sum = 0;
+  while (n != 0) {
+    sum = sum + n % 10;
+    n = n / 10;
+  }
+  return sum;
+}
+
+template<class T=ll>
+T sum_digit_string(T str)
+{
+    T sum = 0;
+    for (T i = 0; i < str.length(); i++)
+    {
+        sum = sum + str[i] - 48;
+    }
+    return sum;
+}
+
+template<class T=ll> string dec2bin(T n)
+{
+    const T size=sizeof(n)*8;
+    string s = "00000000000000000000000000000000";
+    for (T a=0;a<32;a++)
+    {
+        if(n==0)
+            return s;
+        else
+        {
+            if(n%2!=0)
+                s[31-a]='1';
+            n/=2;
+        }
+    }
+    return s;
+}
+
+template<typename T = ll>
+T fac(T x) { // factorial
   T o = 1;
   if (x > 0)
     for(T i = 1; i <= x; ++i)
@@ -109,43 +199,63 @@ T fac(T x) {
   return o;
 }
 
-vl v(N);
-vl par(N, -1);
-vl szz(N);
-vl anc(N);
-bitset<N> vis;
+bool comp2nd(pl& A, pl& B) { return A.S < B.S; }
 
-template<typename T> bool ckmin(T& a, T b){ return b < a ? a = b, true : false; }
-template<typename T> bool ckmax(T& a, T b){ return b > a ? a = b, true : false; }
+void buildAdj(vvl& A, ll nn = 0) {
+  if (!nn) cerr << "::::::::You missed the size arg (\"nn\") while building your adjacency list::::::::" << nl;
+  A.rsz(nn+1, vl {});
+  fo(i,nn) {
+    pl p; cin >> p.F >> p.S;
+    A[p.F].pb(p.S);
+    A[p.S].pb(p.F);
+  }
+}
 
-ll timer = 0;
+// vl v(N);
+// vl p(N, -1);
+// vl szz(N);
+// vl anc(N);
+// bitset<N> vis;
+// bitset<N> bs;
+
+// ll timer = 0;
 // vl tin, tout;
 
-ll n, m, q, k;
+ll a, b, c, n, m, k, w;
+string s;
 
 /* Solution starts here */
-string s, t;
 
 void solution() {
-  cin >> s >> t;
-  const ll n = s.size(), m = t.size();
-  vvl dp(n+1, vl(m+1, 0));
-  
+  string t;  cin >> s >> t;
+  n = s.sz(), m = t.sz();
+  vvl dp(n+1, vl(m+1));
+
   fo(i,n)
     fo(j,m)
-      if (s[i] == t[j])
-        dp[i+1][j+1] = 1 + dp[i][j];
-      else
-        dp[i+1][j+1] = max(dp[i+1][j],dp[i][j+1]);
+      dp[i+1][j+1] = s[i] == t[j] ? 1 + dp[i][j] : max(dp[i+1][j], dp[i][j+1]);
 
-  cout << dp[n][m] << nl;
+  // Start from bottom right corner and store chars in lcs[] 1-by-1
+  ll idx = dp[n][m];
+  string lcs;
+
+  while(m && n)
+    if (dp[n][m] == dp[n][m-1])
+      m--;
+    else if (dp[n][m] == dp[n-1][m])
+      n--;
+    else if (dp[n][m] == dp[n-1][m-1] + 1) {
+      lcs += s[n-1];
+      n--;
+      m--;
+    }
+  reverse(all(lcs));
+  cout << lcs << nl;
 }
 
 int main() {
   ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
   srand(chrono::high_resolution_clock::now().time_since_epoch().count());
-  ll t; cin >> t;
-
   solution();
 
   return 0;
