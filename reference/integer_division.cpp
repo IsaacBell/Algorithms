e249a38c832e1da5a8f53a667ab301a67dfd2ed4
Isaac Bell <isaacbell388@gmail.com>
2021-11-02 12:29:51 -0500

    Add reference code for int division

---
+++ reference/integer_division.cpp
@@ -0,0 +1,12 @@
+/*
+  To calculate ⌈𝑥/𝑦⌉ in integers, you should divide 𝑥+𝑦−1 by 𝑦 using 
+  integer division 
+  
+  (this will work provided that both 𝑥 and 𝑦 are non-negative, 
+  and 𝑦≠0). 
+  
+  If you use real numbers, this may cause precision issues.
+*/
+ll int_div(ll x, ll y) {
+  return (x + y-1) / 2;
+}
