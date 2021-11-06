b6764406437236a4d212e5a4add0139bf1243132
Isaac Bell <isaacbell388@gmail.com>
2021-11-06 16:13:44 -0500

    Practice problems

--- codeforces/problemset/consecutive_sum_riddle.cpp
+++ codeforces/problemset/consecutive_sum_riddle.cpp
@@ -231,40 +231,17 @@ ll a, b, c, n, m, k, w;
 string s;
 
 void solution() {
-  ll x; cin >> n >> x;
-  vector<int> page(n),price(n);
-  for(int&i : price)
-    cin>>i;
-
-  for(int&i : page)
-    cin>>i;
-
-  vector<vector<int>> dp(n+1,vector<int>(x+1,0));
-
-  for(int i=1;i<=n;i++)
-  {
-      for(int j=0;j<=x;j++)
-      {
-          //option1 = book number i-1 is not included, hence no pages added.
-          //option2 = book number i-1 is included, hence pages are added.
-          //option1 = dp[i-1][j] ...book not included so total price(j) remains same.
-          //option2 = dp[i-1][j-price[i-1]] + page[i-1]....total price(j) decreased and pages are counted for book i-1.
-
-          dp[i][j] = dp[i-1][j];
-          if(j >= price[i-1])
-          {
-              dp[i][j] = max (dp[i][j],dp[i-1][j-price[i-1]] + page[i-1]); //max of both the boxes included in dp[i][j].
-          }
-      }
-  }
-
-  cout<< dp[n][x] <<endl;
+  cin >> n;
+  cout << 1-n << " " << n << nl;
 }
 
 int main() {
   ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
   srand(chrono::high_resolution_clock::now().time_since_epoch().count());
-  solution();
+  ll t; cin >> t;
+
+  while(t--)
+    solution();
 
   return 0;
 }
