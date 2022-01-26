1606070bc7eccb879e733e02c24997a9681e17c5
Isaac Bell <isaacbell388@gmail.com>
2022-01-26 14:22:46 -0600

    Update Kadane's algo reference code

--- reference/traversal/kadanes.cpp
+++ reference/traversal/kadanes.cpp
@@ -1,15 +1,32 @@
+/*
+  Standard Kadane's. Only calcs the sum
+*/
+
+int ans = a[0], sum = 0;
+
+fo(r, n) {
+    sum += a[r];
+    ans = max(ans, sum);
+    sum = max(sum, 0);
+}
+
+/*
+  This version maintains the indexes of the boundaries of 
+  the corresponding segment
+*/
+
 int ans = a[0], ans_l = 0, ans_r = 0;
 int sum = 0, minus_pos = -1;
 
-for (int r = 0; r < n; ++r) {
-  sum += a[r];
-  if (sum > ans) {
-    ans = sum;
-    ans_l = minus_pos + 1;
-    ans_r = r;
-  }
-  if (sum < 0) {
-    sum = 0;
-    minus_pos = r;
-  }
+fo (r, n) {
+    sum += a[r];
+    if (sum > ans) {
+        ans = sum;
+        ans_l = minus_pos + 1;
+        ans_r = r;
+    }
+    if (sum < 0) {
+        sum = 0;
+        minus_pos = r;
+    }
 }
