43b0bdc16af237dd60dd7671d4580e12baff14ce
Isaac Bell <isaacbell388@gmail.com>
2022-03-15 01:34:03 -0500

    Update graph template

--- templates/graph.cpp
+++ templates/graph.cpp
@@ -1,6 +1,130 @@
-void solution() {
-  rd(n);
-  auto adj = buildAdj(n, n-1);
-  // dfs(adj, 1);
-  // dfs2(adj, 1);
+/* 
+  Original Author: tourist
+  Edited by Isaac Bell
+*/
+
+template <typename T>
+class graph {
+  public:
+  struct edge {
+    int from;
+    int to;
+    T cost;
+  };
+
+  map<pl,ll> weightDict; // todo - speed this up
+  vector<edge> edges;
+  vector< vector<int> > g;
+  int n;
+
+  function<bool(int)> ignore;
+
+  graph(int _n) : n(_n) {
+    g.resize(n);
+    ignore = nullptr;
+  }
+
+  virtual int add(int from, int to, T cost) = 0;
+
+  virtual void set_ignore_edge_rule(const function<bool(int)> &f) {
+    ignore = f;
+  }
+
+  virtual void unset_ignore_edge_rule() {
+    ignore = nullptr;
+  }
+};
+
+template <typename T>
+class digraph : public graph<T> {
+  public:
+  using graph<T>::weightDict;
+  using graph<T>::edges;
+  using graph<T>::g;
+  using graph<T>::n;
+  using graph<T>::ignore;
+
+  digraph(int _n) : graph<T>(_n) {
+  }
+
+  int add(int from, int to, T cost = 1) {
+    assert(0 <= from && from < n && 0 <= to && to < n);
+    int id = (int) edges.size();
+    g[from].push_back(id);
+    edges.push_back({from, to, cost});
+    weightDict[{from, to}] = cost;
+    return id;
+  }
+
+  digraph<T> reverse() {
+    digraph<T> rev(n);
+    for (auto &e : edges) {
+      rev.add(e.to, e.from, e.cost);
+    }
+    if (ignore != nullptr) {
+      rev.set_ignore_edge_rule([&](int id) {
+        return ignore(id);
+      });
+    }
+    return rev;
+  }
+
+  T dfsMaxSumPath(ll i = 0, ll tar = -1e18, ll o = 0) {
+    if (tar == -1e18) tar = n;
+    if (i == n) return o;
+    trav(nei, g[i])
+      ckmax(o, dfsMaxSumPath(tar, nei, o + weightDict[{i, nei}]) );
+  }
+};
+
+template <typename T>
+class undigraph : public graph<T> {
+  public:
+  using graph<T>::weightDict;
+  using graph<T>::edges;
+  using graph<T>::g;
+  using graph<T>::n;
+
+  undigraph(int _n) : graph<T>(_n) {
+  }
+
+  int add(int from, int to, T cost = 1) {
+    assert(0 <= from && from < n && 0 <= to && to < n);
+    int id = (int) edges.size();
+    g[from].push_back(id);
+    g[to].push_back(id);
+    edges.push_back({from, to, cost});
+    weightDict[{from, to}] = cost;
+    return id;
+  }
+};
+
+template <typename T>
+vector <T> dijkstra(const graph<T> &g, int start) {
+  assert(0 <= start && start < g.n);
+  vector<T> dist(g.n, numeric_limits<T>::max());
+  priority_queue<pair<T, int>, vector<pair<T, int> >, greater<pair<T, int> > > s;
+  dist[start] = 0;
+  s.emplace(dist[start], start);
+  while (!s.empty()) {
+    T expected = s.top().first;
+    int i = s.top().second;
+    s.pop();
+    if (dist[i] != expected) {
+      continue;
+    }
+    for (int id : g.g[i]) {
+      if (g.ignore != nullptr && g.ignore(id)) {
+        continue;
+      }
+      auto &e = g.edges[id];
+      int to = e.from ^ e.to ^ i;
+      if (dist[i] + e.cost < dist[to]) {
+        dist[to] = dist[i] + e.cost;
+        s.emplace(dist[to], to);
+      }
+    }
+  }
+  return dist;
+  // returns numeric_limits<T>::max() if there's no path
 }
