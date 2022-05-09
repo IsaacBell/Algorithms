42c7b3865cb44cf1f2d8525bb57e4007bddc6344
Isaac Bell <isaacbell388@gmail.com>
2022-05-09 15:25:14 -0500

    Permutation Inversions

---
+++ reference/combinatorics/permutation.cpp
@@ -0,0 +1,61 @@
+template <class T = ll>
+struct Permutation {
+  using vt = vector<T>;
+
+  vt A;
+  const ll n;
+
+  Permutation(vt& in): A(in), n(in.size()) {};
+
+  ll inversionCount() {
+    vt A2(all(A));
+    return mergeSort(A, A2, 0, n - 1);
+  }
+
+private:
+  // merge two sorted subarrays `arr[low … mid]` and `arr[mid+1 … high]`
+  ll merge(vl arr, vl aux, ll low, ll mid, ll high) {
+    ll k = low, i = low, j = mid + 1;
+    ll inversionCount = 0;
+
+    // while there are elements in the left and right runs
+    while (i <= mid && j <= high) {
+      if (arr[i] <= arr[j])
+        aux[k++] = arr[i++];
+      else {
+        aux[k++] = arr[j++];
+        inversionCount += (mid - i + 1);
+      }
+    }
+
+    // copy remaining elements
+    while (i <= mid)
+      aux[k++] = arr[i++];
+
+    /* no need to copy the second half (since the remaining items
+    are already in their correct position in the temporary array) */
+
+    // copy back to the original array to reflect sorted order
+    Fo(i, low, high+1)
+      arr[i] = aux[i];
+
+    return inversionCount;
+  }
+
+  // Sort array `arr[low…high]` using auxiliary array `aux`
+  ll mergeSort(vl arr, vl aux, ll low, ll high) {
+    if (high <= low) return 0;
+
+    ll mid = (low + ((high - low) >> 1));
+    ll inversionCount = 0;
+
+    // split
+    inversionCount += mergeSort(arr, aux, low, mid);
+    inversionCount += mergeSort(arr, aux, mid + 1, high);
+
+    // merge halves
+    inversionCount += merge(arr, aux, low, mid, high);
+
+    return inversionCount;
+  }
+};
